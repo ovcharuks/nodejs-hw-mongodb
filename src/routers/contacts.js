e00dfc7573625397d504adcfab33d26b97b0e08c
Сергій Овчарук <ovcharuksvg@gmail.com>
2025-03-23 16:11:21 +0200

    moved router use from routers to server

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,13 +6,4 @@ router.get('/', getContacts);
 
 router.get('/:id', getContactById);
 
-router.use((req, res, next) => {
-  res.status(404).send('Not found');
-});
-
-router.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something not');
-});
-
 export default router;
--- src/server.js
+++ src/server.js
@@ -5,8 +5,18 @@ import contactRoutes from './routers/contacts.js';
 const PORT = 3000;
 const app = express();
 const router = express.Router();
+
 export const setupServer = () => {
   router.use('/contacts', contactRoutes);
+
+  router.use((req, res, next) => {
+    res.status(404).send('Not found');
+  });
+
+  router.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something not');
+  });
 
   app.listen(PORT, () => {
     console.log(`Mongo connection successfully established!`);
