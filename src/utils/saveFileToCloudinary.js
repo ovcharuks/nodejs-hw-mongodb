cd63f5a54ba67ed2ca8c90f34fc6ddcb166525eb
Сергій Овчарук <ovcharuksvg@gmail.com>
2025-04-12 12:20:40 +0300

    2

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,7 +1,8 @@
 import cloudinary from 'cloudinary';
 import fs from 'node:fs/promises';
-import { CLOUDINARY } from '../constants.js';
+
 import { getEnvVar } from './getEnvVar.js';
+import { CLOUDINARY } from '../constants/index.js';
 
 cloudinary.v2.config({
   secure: true,
