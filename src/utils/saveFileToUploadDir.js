67e74b9e5127b42ef25bf848a8ed20ea0e65b018
Сергій Овчарук <ovcharuksvg@gmail.com>
2025-04-12 12:51:31 +0300

    3

--- src/utils/saveFileToUploadDir.js
+++ src/utils/saveFileToUploadDir.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
-import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from '../constants.js';
 import { getEnvVar } from './getEnvVar.js';
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from '../constants/index.js';
 
 export const saveFileToUploadDir = async (file) => {
   await fs.rename(
