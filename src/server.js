f908bbce1b7f0a8b00fffb1e14539bc2c3db339e
Сергій Овчарук <ovcharuksvg@gmail.com>
2025-04-14 00:08:20 +0300

    initial

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routes/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = 3000;
 const app = express();
@@ -16,13 +17,15 @@ export const setupServer = () => {
   // app.use(cors());
   app.use(cookieParser());
   // app.use('/contacts', contactRoutes);
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
 
   app.use(notFoundHandler);
 
   app.use(errorHandler);
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.listen(PORT, () => {
     console.log(`Mongo connection successfully established!`);
